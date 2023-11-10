61c191cc14ce5e30960686efbe2e6a28fc552443
Hiep <winwaywa@gmail.com>
2023-11-10 22:44:55 +0700

    add entity and setting connect db

--- src/main/java/com/blog/api/NewAPI.java
+++ src/main/java/com/blog/api/NewAPI.java
@@ -1,13 +1,27 @@
 package com.blog.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.blog.dto.NewDTO;
+
+//@Controller
+@RestController // = @Controller + @RequestBody
 public class NewAPI {
 
-	@GetMapping
-	public String testAPI() {
-		return "success";
-	}
+//    @RequestMapping(value = "/new", method = RequestMethod.POST)
+//    @ResponseBody
+//    public NewDTO createNew(@RequestBody NewDTO model) {
+//            return model;
+//    }
+    
+    @PostMapping("/new") // @RequestMapping + RequestMethod.POST
+    public NewDTO createNew(@RequestBody NewDTO model) {
+            return model;
+    }
 }
